b9bb290f9b8d775c5ea400e9b13b03cf0632f176
Francisco J. Solis-Munoz <siscomagma@gmail.com>
2020-12-22 14:49:57 -0600

    Modified test case

--- test.cpp
+++ test.cpp
@@ -6,6 +6,6 @@ namespace {
         EXPECT_EQ(9.0, numberToThe(3.0, 2.0));
     }
     TEST(PowerTest, Two) {
-        EXPECT_EQ(5.0, numberToThe(2.0, 2.0));
+        EXPECT_EQ(4.0, numberToThe(2.0, 2.0));
     }
 }
