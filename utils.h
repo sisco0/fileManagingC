8befd1aec13da5dc4b7f86b215d1dc1243de11e0
Francisco J. Solis-Munoz <siscomagma@gmail.com>
2020-12-22 14:56:33 -0600

    Added todo example function

--- utils.h
+++ utils.h
@@ -3,3 +3,8 @@
 double numberToThe(double _base, double _exp) {
     return pow(_base, _exp);
 }
+
+double squareRoot(double num) {
+    // @todo Complete this squareRoot function
+    return 0.0;
+}
